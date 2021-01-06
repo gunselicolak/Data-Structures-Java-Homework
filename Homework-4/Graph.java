9dadd4b9ed0176909813670ff8f0616b3d2601d0
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-01-06 11:25:51 +0300

    Update Graph.java

--- Homework-4/Graph.java
+++ Homework-4/Graph.java
@@ -1,77 +1,52 @@
-//18253069 Günseli ÇOLAK 
+//18253069 GÃ¼nseli Ã‡OLAK 
 public class Odev4Graph<T> extends Odev4AbstractGraph<T> {
     /**
-     * Mevcut çizgenin komþuluk matrisini hesaplayýp geri döndürür
-     * @return Komþuluk matrisi(iki boyutlu tamsayý dizisi)
+     * Mevcut Ã§izgenin komÅŸuluk matrisini hesaplayÄ±p geri dÃ¶ndÃ¼rÃ¼r
+     * @return KomÅŸuluk matrisi(iki boyutlu tamsayÄ± dizisi)
      */
     @Override
     public int[][] adjacencyMatrix() {
-        OdevGraph<T>[] k = Odev4AbstractGraph<>(); 
-        for(Vertex<T> v:vertices)
-         vertices = v;
-        adjacency_matrix = new int[vertices + 1][vertices + 1];
-           
-        return null;
+        int [][] adjacencyMatrix = new int[vertices.size()][vertices.size()];
+        
+        int i = 0;
+        for(Vertex<T> v:vertices){
+            for(Edge<T> e: v.edges){
+                for(int j = 0; j < vertices.size(); j++){
+                    if(e.to == vertices.get(j))
+                        adjacencyMatrix[i][j] = e.weight;
+                }
+            }
+            i++;
+        }
+        return adjacencyMatrix; 
     }
 
     /**
-     * Mevcut çizgenin parametre olarak gönderilen çizge ile eþit olup
-     * olmadýðýný döndürür. Ýki çizgenin birbirine eþit olmasý için düðümlerin
-     * (vertex) referanslarý farklý olsa bile deðerlerinin eþit olmasý ve de
-     * tüm kenarlarýn her iki çizgede de ayný düðümler arasýnda olmasý gerekir
-     * @param g2 Eþitlik kontrolü için gönderilen çizge parametresi
-     * @return Eþit için true aksi halde false döner
+     * Mevcut Ã§izgenin parametre olarak gÃ¶nderilen Ã§izge ile eÅŸit olup
+     * olmadÄ±ÄŸÄ±nÄ± dÃ¶ndÃ¼rÃ¼r. Ä°ki Ã§izgenin birbirine eÅŸit olmasÄ± iÃ§in dÃ¼ÄŸÃ¼mlerin
+     * (vertex) referanslarÄ± farklÄ± olsa bile deÄŸerlerinin eÅŸit olmasÄ± ve de
+     * tÃ¼m kenarlarÄ±n her iki Ã§izgede de aynÄ± dÃ¼ÄŸÃ¼mler arasÄ±nda olmasÄ± gerekir
+     * @param g2 EÅŸitlik kontrolÃ¼ iÃ§in gÃ¶nderilen Ã§izge parametresi
+     * @return EÅŸit iÃ§in true aksi halde false dÃ¶ner
      */
     @Override
     public boolean equals(Odev4AbstractGraph<T> g2) {
-         for (Vertex<T> v:vertices)
-            for(Edge<T> e:v.edges)
-                if( v.value == g2.v.value)// referans farklý olsa bile deðerleri eþit olmalý
-                    if(e.from.value.eguals(g2.e.to.value) || e.to.value.equals(g2.e.from.value)){
-                else
-                    return false;
+          int graph[][] = g2.adjacencyMatrix();
+        int adjgraph[][] = adjacencyMatrix();
+        
+        if(g2.vertices.size() != vertices.size()){
+            return false;
+        }
+        else
+        {
+            for(int i = 0; i < graph.length; i++){
+                for(int j = 0; j < graph.length; j++){
+                    if(graph[i][j] != adjgraph[i][j]){
+                        return false;
                 }
-        return true;
+            }
+        }
     }
-    
-    public int inDegree(int node){ 
-    int sayi=0;
-    for(int i=0;i<komsulukMatrisi.length;i++) 
-        if(komsulukMatrisi[i][node]==1) 
-           sayi++;
-        return sayi;
-    } 
-        
-    public int outDegree(int node){ 
-    int sayi=0; 
-    for(int j=0;j<komsulukMatrisi.length;j++) 
-        if(komsulukMatrisi[node][j]==1) 
-           sayi++; 
-        return sayi;
+    return true;
     }
-
-    
-    
- /*   @Override
-    public int outDegree(T deger) {
-        Vertex<T> v = verticesMap.get(deger);
-        if(v == null)
-            return -1;
-        
-        return v.edges.size();
-    }
-
-    @Override
-    public int inDegree(T deger) {
-        Vertex v2 = verticesMap.get(deger);
-        if(v2 == null)
-            return -1;
-        int indeg = 0;
-        for(Vertex <T> v : vertices)
-           for(Edge<T> e : v.edges)
-              if(e.to.value.equals(deger))
-              indeg ++;
-        return indeg;     
-    }
-    */
 }
