b2d4127b69a28ea8bc261ed3bd25a536dd93b2f4
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-01-06 12:00:08 +0300

    Update Ogrenci.java

--- Homework-2/Ogrenci.java
+++ Homework-2/Ogrenci.java
@@ -1,60 +1,123 @@
-//18253069 Günseli ÇOLAK
-public class Odev2Ogrenci {
+//18253069 GÃ¼nseli Ã‡OLAK
+
+import java.util.Stack;
+
+public class Odev2 {
+    
+    public static String result;
+    
     /**
-     * isBST metodu kökü verilen herhangi bir aðacýn ikili arama aðacý olup
-     * olmadýðýný hesaplar
-     * @param node Kök düðüm
-     * @param <T> karþýlaþtýrýlabilir generic tür
-     * @return ikili arama aðacý mý deðil mi
-     */
-    public static <T extends Comparable<T>> boolean isBST(BTNode<T> node) {
-      
-       /* while(node != null){
-            if(node.value.compareTo(node.left.value) > 0){//sola gidip karþýlaþtýrýr
-                if(node.left.value == null){//en sola geldiðimizin kontrolü
-                node.left = node;
-                node.value = node.left.value;//ebeveyni deðiþtirmeye çalýþtým
-                }
-                else isBST(node.left);// en sola gitmek için fonksiyonumuzu recursive olarak çaðýrýyor
-                return false;
+    * Postfix biÃ§iminde verilen metni ExpressionTree sÄ±nÄ±fÄ±na dÃ¶nÃ¼ÅŸtÃ¼rÃ¼r
+    * @param girdi postfix girdi metni
+    * @return OluÅŸturulan ifade aÄŸacÄ±
+    */
+	public static ExpressionTree postfixToExpressionTree(String girdi) throws Exception {
+		
+        Stack<BTNode> stack = new Stack<>();
+
+        String[] stringArray = girdi.split(" ");
+
+        for (String string : stringArray) {
+            if (Character.isDigit(string.charAt(0))){
+                stack.push(new BTNode(string, null, null));
             }
-            
-            else if(node.value.compareTo(node.right.value) <= 0){//saða gidip karþýlaþtýrýyor
-                if(node.right.value == null){//en saða geldiðimizin kontrolü
-                node.right = node;
-                node.value = node.right.value;
-                }
-                else isBST(node.right);// en saða gitmek için fonksiyonumuzu recursive olarak çaðýrýyor
-                return false;
+            else if (isOperator(string)){
+
+                BTNode temp1 = stack.pop(); // right
+                BTNode temp2 = stack.pop(); //left
+
+                stack.push(new BTNode(string, temp2, temp1));
             }
-            return false;//while için döngü
-        }
-        */
-            if(node == null){
-                //node = node.value;  
-                if(node.value.compareTo(node.left.value) < 0){//sola gidip karþýlaþtýrýr
-                
-                if(node.left.value == null){
-                node.left = node;  
-                //node.parent = node.left;//ebeveyni döndürür
-                }
-                else isBST(node.left);
-                return false;
-            }
-                else if(node.value.compareTo(node.right.value) >= 0){
-               
-                if(node.right.value == null){
-                node.right = node;
-                //node.parent = node.right;
-                }
-                else isBST(node.right);
-                return false;
-            }
-                return false;//if için döngü
-            }
-            
-                     
-        return true;
+            else{
+                throw new Exception("The string is not a number or an operator.");
             }
         }
+        return new ExpressionTree(stack.pop());
+	}
+	
+	public static boolean isOperator(String string) {
+
+		switch (string) {	
+		    case "^":
+			case "*":
+			case "/":
+			case "+":
+			case "-":	
+				return true;
+			default:
+				return false;
+		}
+	}
+	
+    /**
+    * Parametre olarak verilen ifade aÄŸacÄ±nÄ± deÄŸerlendirir(sonucunu hesaplar).
+    * @param agac Ä°fade aÄŸacÄ±
+    * @return Ä°fade aÄŸacÄ±nÄ±n sonucu
+    */
+	public static int evaluateExpressionTree(ExpressionTree agac) {
+		
+		BTNode root = agac.getRoot();
+		
+		result = "";
+		
+		postOrderRoot(root);
+
+		return postfixDegerlendir(result);
+	}
+	
+	public static void postOrderRoot(BTNode root) {
+		
+		if (root.left != null)
+			postOrderRoot(root.left);
+		
+		if (root.right != null)
+			postOrderRoot(root.right);
+		
+		result += root.value + " ";	
+	}
+	
+	public static int postfixDegerlendir(String girdi) {
+		
+		Stack<Integer> pstack =new Stack<>();
+
+        String[] stringArray = girdi.split(" ");
         
+        int result =0;
+        
+        for (String string : stringArray) {
+
+            if (Character.isDigit(string.charAt(0))){
+                pstack.push(Integer.parseInt(string));
+            }
+            else{
+
+                int d1=pstack.pop();
+                int d2=pstack.pop();
+
+                switch (string) {
+
+                    case "^":
+                        result = d2 ^ d1;
+                        break;
+                    case "*":
+                        result = d2 * d1;
+                        break;
+                    case "/":
+                        result = d2 / d1;
+                        break;
+                    case "+":
+                        result = d2 + d1;
+                        break;
+                    case "-":
+                        result = d2 - d1;
+                        break;
+                    default:
+                        break;
+                }
+                pstack.push(result);
+            }
+
+        }
+        return pstack.pop();
+	}
+}
